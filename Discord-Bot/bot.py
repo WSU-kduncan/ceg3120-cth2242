f44c13c1750fe6f3afb9faa6876f4d2f3c207cd7
Corbin Hulin <hulin.3@wright.edu>
2022-09-12 17:55:20 -0400

    asfndad; kfs

--- Discord-Bot/bot.py
+++ Discord-Bot/bot.py
@@ -1,3 +1,4 @@
+# Based on example https://discordpy.readthedocs.io/en/stable/quickstart.html
 import os
 
 import discord
@@ -8,12 +9,9 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 load_dotenv()
-#print(os.getenv('DISCORD_TOKEN'))
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-
-#client = discord.Client()
 client = discord.Client(intents=intents)
 
 @client.event
@@ -34,25 +32,14 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    brooklyn_99_quotes = [
-        'I\'m the human form of the 💯 emoji.',
-        'Bingpot!',
-        (
-            'Cool. Cool cool cool cool cool cool cool, '
-            'no doubt no doubt no doubt no doubt.'
-        ),
-    ]
-
     hitchhiker_quotes = [
         'There is an art, it says, or rather, a knack to flying. The knack lies in learning how to throw yourself at the ground and miss.',
         'It is a mistake to think you can solve any major problems just with potatoes.',
         'In the beginning the Universe was created. This has made a lot of people very angry and been widely regarded as a bad move.',
         'A common mistake that people make when trying to design something completely foolproof is to underestimate the ingenuity of complete fools.',
     ]
-
     if message.content == 'towel!':
-        print("Message recieved")
-        #response = random.choice(brooklyn_99_quotes)
+    #if message.content.startswith('$towel'):
         response = random.choice(hitchhiker_quotes)
         await message.channel.send(response)
 
